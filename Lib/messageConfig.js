f56195b161ce92733aa6ec4fed26c448f21cd362
envoyb178-commits <envoyb178@gmail.com>
2026-08-16 08:35:24 +0200

    Add files via upload

---
+++ Lib/messageConfig.js
@@ -0,0 +1,13 @@
+module.exports = {
+    channelInfo: {
+        contextInfo: {
+            forwardingScore: 999,
+            isForwarded: true,
+            forwardedNewsletterMessageInfo: {
+                newsletterJid: '120363404224384403@newsletter',
+                newsletterName: 'EXON X𝗗 𝗕𝗢𝗧',
+                serverMessageId: -1
+            }
+        }
+    }
+};
